358661c6bfe4375f26aeb04c6b3e486a01318d4c
panghyuk <okh9712@gmail.com>
2022-05-12 11:39:26 +0900

    5th week

--- main/models.py
+++ main/models.py
@@ -3,10 +3,12 @@ from django.db import models
 # Create your models here.
 
 class Blog(models.Model):
+    id = models.AutoField(primary_key = True)
     title = models.CharField(max_length=200)
     writer=models.CharField(max_length=100)
     pub_date = models.DateTimeField()
     body = models.TextField()
+    image = models.ImageField(upload_to = 'blog/',blank = True, null = True)
 
     def __str__(self):
         return self.title
